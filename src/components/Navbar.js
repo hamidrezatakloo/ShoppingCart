b594cc46244a0904a152c74a058e53bfe9734c42
hamidreza <takloohamidreza@gmail.com>
2022-10-24 23:13:27 +0330

    fix: set center navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav
       className="grid grid-cols-12 w-11/12 mx-auto my-8 bg-primary bg-clip-text text-transparent 
-    md:bg-none md:bg-clip-border md:text-black"
+    md:bg-none md:bg-clip-border md:text-black items-center"
     >
       <Link to="." className="font-[Monoton] text-3xl col-span-4">
         Figure
