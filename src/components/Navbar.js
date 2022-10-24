eb074db6e571c14e027204fa158fe228359eb379
hamidreza <takloohamidreza@gmail.com>
2022-10-24 22:22:31 +0330

    fix: Hidden list in navbar in low screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,13 +9,22 @@ const Navbar = () => {
       <Link to="." className="font-[Monoton] text-3xl col-span-4">
         Figure
       </Link>
-      <Link to="." className="font-[Poppins] col-span-2 text-xl">
+      <Link
+        to="."
+        className="font-[Poppins] col-span-2 text-xl hidden md:block"
+      >
         Home
       </Link>
-      <Link to="/Store" className="font-[Poppins] col-span-2 text-xl">
+      <Link
+        to="/Store"
+        className="font-[Poppins] col-span-2 text-xl hidden md:block"
+      >
         Store
       </Link>
-      <Link to="/About" className="font-[Poppins] col-span-2 text-xl">
+      <Link
+        to="/About"
+        className="font-[Poppins] col-span-2 text-xl hidden md:block"
+      >
         About
       </Link>
     </nav>
