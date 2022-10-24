3c444a21da1dc21a4a8dcbb5c779563c6dbe241e
hamidreza <takloohamidreza@gmail.com>
2022-10-24 21:23:49 +0330

    fix: Add vertical gap for different screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-12 w-11/12 mx-auto flex-1 md:content-center">
+    <div className="grid grid-cols-12 w-11/12 mx-auto flex-1 md:content-center md:gap-y-[100px] 2xl:gap-y-[80px]">
       <h1 className="col-span-12 md:col-span-6 font-PassionOne  capitalize md:text-[50px] text-[13vw] lg:text-[70px] xl:text-[80px] 2xl:text-[96px]">
         Buy your favorite character action figures here
       </h1>
