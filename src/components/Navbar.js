78d033cf8ccb8c878fe66f4ef29084d71c2cb341
hamidreza <takloohamidreza@gmail.com>
2022-10-24 15:34:04 +0330

    fix: add font,grid-col and size to nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,19 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="."> Home</Link>
-      <Link to="/Store"> Store</Link>
-      <Link to="/About"> About</Link>
+    <nav className="grid grid-cols-12 w-11/12 mx-auto">
+      <Link to="." className="font-[Monoton] text-3xl col-span-4">
+        Figure
+      </Link>
+      <Link to="." className="font-[Poppins] col-span-2 text-xl">
+        Home
+      </Link>
+      <Link to="/Store" className="font-[Poppins] col-span-2 text-xl">
+        Store
+      </Link>
+      <Link to="/About" className="font-[Poppins] col-span-2 text-xl">
+        About
+      </Link>
     </nav>
   );
 };
