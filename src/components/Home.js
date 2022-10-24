e4b8b5c9a2c0c812593019b2535681d0e2428a97
hamidreza <takloohamidreza@gmail.com>
2022-10-24 19:57:10 +0330

    fix: Hidden image in low-screens and h1 full-width

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 const Home = () => {
   return (
     <div className="grid grid-cols-12 w-11/12 mx-auto">
-      <h1 className="col-span-6 font-PassionOne  capitalize md:text-[50px] text-[13vw] lg:text-[70px] xl:text-[80px] 2xl:text-[96px]">
+      <h1 className="col-span-12 md:col-span-6 font-PassionOne  capitalize md:text-[50px] text-[13vw] lg:text-[70px] xl:text-[80px] 2xl:text-[96px]">
         Buy your favorite character action figures here
       </h1>
       <img
         alt="ActionFigure"
         src="./kakashi_LEE.webp"
-        className="col-start-7 col-span-6 "
+        className="hidden md:block col-start-7 col-span-6 "
       />
     </div>
   );
