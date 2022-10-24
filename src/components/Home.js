beee9a89414c7ad83489ad36bc9795813d96da6a
hamidreza <takloohamidreza@gmail.com>
2022-10-24 22:15:11 +0330

    feat: Set linear-gradient for screen devices

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="grid grid-cols-12 w-11/12 mx-auto flex-1 md:content-center md:gap-y-[100px] 2xl:gap-y-[80px]">
-      <h1 className="col-span-12 md:col-span-6 font-PassionOne  capitalize md:text-[50px] text-[13vw] lg:text-[70px] xl:text-[80px] 2xl:text-[96px]">
+      <h1
+        className="col-span-12 md:col-span-6 font-PassionOne bg-primary bg-clip-text text-transparent md:text-black md:bg-none md:bg-clip-border 
+      capitalize md:text-[50px] text-[13vw] lg:text-[70px] xl:text-[80px] 2xl:text-[96px]"
+      >
         Buy your favorite character action figures here
       </h1>
       <img
@@ -13,7 +16,8 @@ const Home = () => {
       />
       <Link
         to="/store"
-        className="self-start capitalize text-white bg-black py-4 px-8 w-fit whitespace-nowrap rounded text-lg font-semibold font-[Poppins]"
+        className="self-start capitalize bg-primary text-white py-4 px-8 w-fit whitespace-nowrap rounded text-lg font-semibold font-[Poppins]
+         md:bg-none md:bg-black"
       >
         go to store
       </Link>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="grid grid-cols-12 w-11/12 mx-auto">
+    <nav
+      className="grid grid-cols-12 w-11/12 mx-auto my-8 bg-primary bg-clip-text text-transparent 
+    md:bg-none md:bg-clip-border md:text-black"
+    >
       <Link to="." className="font-[Monoton] text-3xl col-span-4">
         Figure
       </Link>
