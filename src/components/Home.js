120a606feed97dc8cf40e402a88f4de19a5c670b
hamidreza <takloohamidreza@gmail.com>
2022-10-24 19:49:08 +0330

    feat: Add image and header to Home component
    header and image both have half o full-width

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div className="grid grid-cols-12 w-11/12 mx-auto"></div>;
+  return (
+    <div className="grid grid-cols-12 w-11/12 mx-auto">
+      <h1 className="col-span-6 font-PassionOne  capitalize">
+        Buy your favorite character action figures here
+      </h1>
+      <img
+        alt="ActionFigure"
+        src="./kakashi_LEE.webp"
+        className="col-start-7 col-span-6 "
+      />
+    </div>
+  );
 };
 
 export default Home;
