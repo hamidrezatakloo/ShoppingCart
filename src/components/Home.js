372e35516b2986618a55bbf4ae0cb5dd7b87eff1
hamidreza <takloohamidreza@gmail.com>
2022-10-24 20:32:01 +0330

    fix: set all of freeSpace as height of Home
    We do this with flex-1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="grid grid-cols-12 w-11/12 mx-auto">
+    <div className="grid grid-cols-12 w-11/12 mx-auto flex-1">
       <h1 className="col-span-12 md:col-span-6 font-PassionOne  capitalize md:text-[50px] text-[13vw] lg:text-[70px] xl:text-[80px] 2xl:text-[96px]">
         Buy your favorite character action figures here
       </h1>
