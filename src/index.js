9b182b2636626355ca92198594571f24d5fdc73f
hamidreza <takloohamidreza@gmail.com>
2022-10-24 15:09:06 +0330

    refactor: Use BrowserRouter ,Routes,Route

--- src/index.js
+++ src/index.js
@@ -2,29 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Store from "./components/Store";
 import About from "./components/About";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/Store",
-    element: <Store />,
-  },
-  {
-    path: "/About",
-    element: <About />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Store" element={<Store />}></Route>
+        <Route path="/About" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
