0393fc6ae37bb05a6ffc12a4498c70429d822aab
hamidreza <takloohamidreza@gmail.com>
2022-10-25 13:01:31 +0330

    fix: set font PassionOne for title

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = ({ src, title, price }) => {
   return (
     <div className="col-span-4">
       <img alt={title} src={src} />
-      <h2>{title}</h2>
+      <h2 className="font-PassionOne">{title}</h2>
       <div>{price}</div>
       <button>Add To Card</button>
     </div>
