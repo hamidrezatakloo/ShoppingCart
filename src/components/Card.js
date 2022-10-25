f5df518a7c904cbb6d3d411decb18573eac0a1ba
hamidreza <takloohamidreza@gmail.com>
2022-10-25 16:09:00 +0330

    fix: Remove justify-around and use margins

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 const Card = ({ src, title, price }) => {
   return (
-    <div className="col-span-12  sm:col-span-6 md:col-span-6 lg:col-span-4 flex flex-col justify-around items-center">
+    <div className="col-span-12  sm:col-span-6 md:col-span-6 lg:col-span-4 flex flex-col items-center">
       <img alt={title} src={src} className="h-[400px] xl:h-[500px]" />
-      <h2 className="font-PassionOne text-4xl text-center w-10/12">{title}</h2>
-      <p className="text-rose-800 text-3xl font-Poppins">
+      <h2 className="font-PassionOne text-4xl text-center w-10/12 my-2">
+        {title}
+      </h2>
+      <p className="text-rose-800 text-3xl font-Poppins my-4">
         <sup>$</sup>
         {price}
       </p>
-      <button className="bg-emerald-500 py-2 px-8 text-white font-Poppins text-lg font-bold">
+      <button className="bg-emerald-500 py-2 px-8 text-white font-Poppins text-lg font-bold my-4">
         Add To Card
       </button>
     </div>
