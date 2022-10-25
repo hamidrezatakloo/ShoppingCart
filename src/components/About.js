473a6f2ea10538227077731b317df3a4cbb9e4f8
hamidreza <takloohamidreza@gmail.com>
2022-10-25 18:56:52 +0330

    fix: Set center items column base with flex

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1">
       <img alt="actionFigure" src="./about.webp" className="col-span-6" />
-      <div className="col-span-6">
+      <div className="col-span-6 flex justify-center items-center flex-col gap-20">
         <h2 className="font-PassionOne text-4xl">what is an action figure</h2>
         <p className="font-[Poppins] text-2xl capitalize">
           An action figure is a poseable character model figure made most
