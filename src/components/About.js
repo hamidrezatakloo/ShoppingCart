f69e77063577e2d94e08171575a2304f3a61c4e2
hamidreza <takloohamidreza@gmail.com>
2022-10-25 18:49:43 +0330

    fix: Set font and size of header

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
     <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1">
       <img alt="actionFigure" src="./about.webp" className="col-span-6" />
       <div className="col-span-6">
-        <h2>what is an action figure</h2>
+        <h2 className="font-PassionOne text-4xl">what is an action figure</h2>
         <p>
           An action figure is a poseable character model figure made most
           commonly of plastic, and often based upon characters from a film,
