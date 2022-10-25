724004a1f402eeffec38e61dfb24da01d936616e
hamidreza <takloohamidreza@gmail.com>
2022-10-25 13:10:34 +0330

    fix: Set font-size and font-family for price

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ const Card = ({ src, title, price }) => {
     <div className="col-span-4">
       <img alt={title} src={src} />
       <h2 className="font-PassionOne text-4xl">{title}</h2>
-      <div className="text-rose-800">{price}</div>
+      <div className="text-rose-800 text-3xl font-Poppins">{price}</div>
       <button>Add To Card</button>
     </div>
   );
