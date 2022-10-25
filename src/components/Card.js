5176f6c6efb3a5ef333ef51312eb58ba9a37f6cf
hamidreza <takloohamidreza@gmail.com>
2022-10-25 16:11:05 +0330

    fix: change title to uppercase

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = ({ src, title, price }) => {
   return (
     <div className="col-span-12  sm:col-span-6 md:col-span-6 lg:col-span-4 flex flex-col items-center">
       <img alt={title} src={src} className="h-[400px] xl:h-[500px]" />
-      <h2 className="font-PassionOne text-4xl text-center w-10/12 my-2">
+      <h2 className="font-PassionOne text-4xl text-center w-10/12 my-2 uppercase">
         {title}
       </h2>
       <p className="text-rose-800 text-3xl font-Poppins my-4">
