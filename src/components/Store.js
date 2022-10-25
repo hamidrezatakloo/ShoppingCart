b411908c3fef63bcf76f600707a2125e1d98b46a
hamidreza <takloohamidreza@gmail.com>
2022-10-25 17:47:03 +0330

    fix: use flex to get all of freeSpace

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,5 +1,7 @@
 const Store = () => {
-  return <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px]"></div>;
+  return (
+    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1"></div>
+  );
 };
 
 export default Store;
