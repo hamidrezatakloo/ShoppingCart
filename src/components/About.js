eb14cf568527f2e88ce9e5fd686d70003223127a
hamidreza <takloohamidreza@gmail.com>
2022-10-25 18:58:28 +0330

    fix: Move capitalize to container div

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ const About = () => {
   return (
     <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1">
       <img alt="actionFigure" src="./about.webp" className="col-span-6" />
-      <div className="col-span-6 flex justify-center items-center flex-col gap-20">
+      <div className="col-span-6 flex justify-center items-center flex-col gap-20 capitalize">
         <h2 className="font-PassionOne text-4xl">what is an action figure</h2>
-        <p className="font-[Poppins] text-2xl capitalize">
+        <p className="font-[Poppins] text-2xl">
           An action figure is a poseable character model figure made most
           commonly of plastic, and often based upon characters from a film,
           comic book, military, video game or television program; fictional or
