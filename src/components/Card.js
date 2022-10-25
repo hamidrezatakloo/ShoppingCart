480f8750e99dcd2280df8b8f951537ac0a3d6749
hamidreza <takloohamidreza@gmail.com>
2022-10-25 12:48:40 +0330

    fix: Add missed return in component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 const Card = ({ src, title, price }) => {
-  <div>
-    <img alt={title} src={src} />
-    <h2>{title}</h2>
-    <div>{price}</div>
-    <button>Add To Card</button>
-  </div>;
+  return (
+    <div>
+      <img alt={title} src={src} />
+      <h2>{title}</h2>
+      <div>{price}</div>
+      <button>Add To Card</button>
+    </div>
+  );
 };
 
 export default Card;
