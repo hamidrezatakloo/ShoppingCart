fd4231d6f8ff6ada890cc5bb14a2f863076a022b
hamidreza <takloohamidreza@gmail.com>
2022-10-25 17:52:03 +0330

    fix: add justify-around to cards have same space

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ src, title, price }) => {
   return (
-    <div className="col-span-12  sm:col-span-6 md:col-span-6 lg:col-span-4 flex flex-col items-center">
+    <div className="col-span-12  sm:col-span-6 md:col-span-6 lg:col-span-4 flex flex-col items-center justify-around">
       <img alt={title} src={src} className="h-[400px] xl:h-[500px]" />
       <h2 className="font-PassionOne text-4xl text-center w-10/12 my-2 uppercase">
         {title}
