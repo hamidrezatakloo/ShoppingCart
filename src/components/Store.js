467422bb1cd3aa21c7c71ca9110ec6dafc9ca9ea
hamidreza <takloohamidreza@gmail.com>
2022-10-25 12:52:19 +0330

    fix: add gap between columns in store container

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,5 +1,5 @@
 const Store = () => {
-  return <div className="grid grid-cols-12 w-11/12 mx-auto"></div>;
+  return <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px]"></div>;
 };
 
 export default Store;
