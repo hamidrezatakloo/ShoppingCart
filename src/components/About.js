a21dfea5094de1fbb62cc3acde744e3a45d180a2
hamidreza <takloohamidreza@gmail.com>
2022-10-25 18:54:14 +0330

    fix: Set size,font and capitalize text

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
       <img alt="actionFigure" src="./about.webp" className="col-span-6" />
       <div className="col-span-6">
         <h2 className="font-PassionOne text-4xl">what is an action figure</h2>
-        <p>
+        <p className="font-[Poppins] text-2xl capitalize">
           An action figure is a poseable character model figure made most
           commonly of plastic, and often based upon characters from a film,
           comic book, military, video game or television program; fictional or
