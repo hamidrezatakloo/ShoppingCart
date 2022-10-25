990e316e16c8cd631c6cecdefd1bf6171583eac7
hamidreza <takloohamidreza@gmail.com>
2022-10-25 12:19:45 +0330

    feat: Add custom animation in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         PassionOne: "Passion One",
       },
+      keyframes: {
+        drop: {
+          "0%": { width: "0" },
+          "100%": { width: "66.666667%" },
+        },
+      },
+      animation: {
+        drop: "drop 1s ease-out forwards",
+      },
     },
   },
   plugins: [],
