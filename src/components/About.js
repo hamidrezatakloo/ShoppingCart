a6efc7c57ec4b69176a7c052abc6b3d5d130a700
hamidreza <takloohamidreza@gmail.com>
2022-10-25 18:44:37 +0330

    feat: Add img and div each have half of container
    div have a header tag and a disc about Action Figures

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,18 @@
 const About = () => {
   return (
-    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1"></div>
+    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1">
+      <img alt="actionFigure" src="./about.webp" className="col-span-6" />
+      <div className="col-span-6">
+        <h2>what is an action figure</h2>
+        <p>
+          An action figure is a poseable character model figure made most
+          commonly of plastic, and often based upon characters from a film,
+          comic book, military, video game or television program; fictional or
+          historical. These figures are usually marketed toward boys and adult
+          collectors.
+        </p>
+      </div>
+    </div>
   );
 };
 
