db1b54450b54803d72973d8341e7599553591596
hamidreza <takloohamidreza@gmail.com>
2022-10-25 17:48:23 +0330

    feat: Render card for each data

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,6 +1,13 @@
+import Card from "./Card";
+import data from "./../data.json";
+
 const Store = () => {
   return (
-    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1"></div>
+    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1">
+      {data.map((d, i) => (
+        <Card key={i} src={d.src} title={d.title} price={d.price} />
+      ))}
+    </div>
   );
 };
 
