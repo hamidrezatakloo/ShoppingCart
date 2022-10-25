5f11d612b7c680032353f9395f16d82548eca318
hamidreza <takloohamidreza@gmail.com>
2022-10-25 13:28:24 +0330

    fix: Add $ sign before price

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,10 @@ const Card = ({ src, title, price }) => {
     <div className="col-span-4">
       <img alt={title} src={src} />
       <h2 className="font-PassionOne text-4xl">{title}</h2>
-      <p className="text-rose-800 text-3xl font-Poppins">{price}</p>
+      <p className="text-rose-800 text-3xl font-Poppins">
+        <sup>$</sup>
+        {price}
+      </p>
       <button>Add To Card</button>
     </div>
   );
