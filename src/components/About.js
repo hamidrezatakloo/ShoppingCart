c9057aadb5996200a5aefbe66f180b77abcc38c1
hamidreza <takloohamidreza@gmail.com>
2022-10-25 19:06:27 +0330

    fix: increase line-height

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
       <img alt="actionFigure" src="./about.webp" className="col-span-6" />
       <div className="col-span-6 flex justify-center items-center flex-col gap-20 capitalize">
         <h2 className="font-PassionOne text-5xl">what is an action figure</h2>
-        <p className="font-[Poppins] text-2xl">
+        <p className="font-[Poppins] text-2xl leading-10">
           An action figure is a poseable character model figure made most
           commonly of plastic, and often based upon characters from a film,
           comic book, military, video game or television program; fictional or
