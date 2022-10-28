7d4d946702bb376f9a4f1b64599128e8eace6b51
hamidreza <takloohamidreza@gmail.com>
2022-10-28 22:20:37 +0330

    fix: Add $ before price in item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,7 @@ const Item = ({ src, title, price, number }) => {
     <div className="col-span-12 flex items-center p-4 justify-around bg-primary text-white font-[Poppins] font-bold rounded">
       <img alt="actionFigure" src={src} className="w-[100px] mx-4" />
       <h4 className="px-4 text-5xl capitalize">{title}</h4>
-      <p className="px-4 text-2xl">{price}</p>
+      <p className="px-4 text-2xl">${price}</p>
       <p className="px-4 text-2xl">
         Number:<span>&nbsp;{number}</span>
       </p>
