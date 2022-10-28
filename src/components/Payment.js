14d47c46a845ae0d5f6c6c2a15afa6d93f29cbdd
hamidreza <takloohamidreza@gmail.com>
2022-10-28 20:47:28 +0330

    fix: Set container width and margin and ...

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,3 +1,7 @@
-const Payment = (props) => {};
+const Payment = (props) => {
+  return (
+    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1 items-start"></div>
+  );
+};
 
 export default Payment;
