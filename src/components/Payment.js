cbc93b327b7d3a0eccb82ed88b14e5c7990b069b
hamidreza <takloohamidreza@gmail.com>
2022-10-28 22:18:35 +0330

    fix: Render shops with item component

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,6 +1,17 @@
-const Payment = (props) => {
+import Item from "./Item";
+const Payment = ({ shops }) => {
   return (
-    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1 items-start"></div>
+    <div className="grid grid-cols-12 w-11/12 mx-auto gap-[20px] flex-1 items-start">
+      {shops.map((it, i) => (
+        <Item
+          key={i}
+          src={it.src}
+          title={it.title}
+          price={it.price}
+          number={it.number}
+        />
+      ))}
+    </div>
   );
 };
 
