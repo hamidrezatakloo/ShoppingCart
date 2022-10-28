5ad1a0fe53c2367bf36c8483e3f39d5eea72f3d8
hamidreza <takloohamidreza@gmail.com>
2022-10-28 22:23:16 +0330

    fix: Set height instead width

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
 const Item = ({ src, title, price, number }) => {
   return (
     <div className="col-span-12 flex items-center p-4 justify-around bg-primary text-white font-[Poppins] font-bold rounded">
-      <img alt="actionFigure" src={src} className="w-[100px] mx-4" />
+      <img alt="actionFigure" src={src} className="h-[300px] mx-4" />
       <h4 className="px-4 text-5xl capitalize">{title}</h4>
       <p className="px-4 text-2xl">${price}</p>
       <p className="px-4 text-2xl">
