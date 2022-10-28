789352285ef65cfdecbd330ba6daf3fafdfe0edb
hamidreza <takloohamidreza@gmail.com>
2022-10-28 22:39:04 +0330

    fix: Add image in a container

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,10 @@
 const Item = ({ src, title, price, number }) => {
   return (
-    <div className="col-span-12 flex items-center p-4 justify-around bg-primary text-white font-[Poppins] font-bold rounded">
-      <img alt="actionFigure" src={src} className="h-[300px] mx-4" />
-      <h4 className="px-4 text-5xl capitalize">{title}</h4>
+    <div className="col-span-12 flex items-center p-4 justify-between bg-primary text-white font-[Poppins] font-bold rounded">
+      <div className=" w-[300px] grid place-items-center shrink-0">
+        <img alt="actionFigure" src={src} className="h-[300px]" />
+      </div>
+      <h4 className="text-5xl capitalize">{title}</h4>
       <p className="px-4 text-2xl">${price}</p>
       <p className="px-4 text-2xl">
         Number:<span>&nbsp;{number}</span>
